9a15669ed96d019746ee7452d1b211f4d08ff26e
Simen <Simen.ks@hotmail.com>
2019-08-29 13:46:02 +0200

    removed index preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,13 +2,11 @@ import CMS from "netlify-cms-app";
 import uploadcare from "netlify-cms-media-library-uploadcare";
 import cloudinary from "netlify-cms-media-library-cloudinary";
 
-import IndexPagePreview from "./preview-templates/IndexPagePreview";
 import ProductPagePreview from "./preview-templates/ProductPagePreview";
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
-CMS.registerPreviewTemplate("index", IndexPagePreview);
 CMS.registerPreviewTemplate("liftutleie", ProductPagePreview);
 CMS.registerPreviewTemplate("aluminiumsrenner", ProductPagePreview);
 CMS.registerPreviewTemplate("automatisk-industrimaskin", ProductPagePreview);
